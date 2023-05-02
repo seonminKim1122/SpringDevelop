369c28a2b2e5a7ba1f706d562f4418ec746c45aa
Kim Seonmin <w.hand715@gmail.com>
2023-05-02 19:31:16 +0900

    swagger-ui 적용

--- src/main/java/com/example/springdevelop/config/WebSecurityConfig.java
+++ src/main/java/com/example/springdevelop/config/WebSecurityConfig.java
@@ -36,6 +36,7 @@ public class WebSecurityConfig {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         // h2-console 사용 및 resources 접근 허용
+        // swagger-ui/index.html 접근 허용
         return (web) -> web.ignoring()
                 .requestMatchers(PathRequest.toH2Console())
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
@@ -52,11 +53,12 @@ public class WebSecurityConfig {
                 .requestMatchers("/user/signup").permitAll()
                 .requestMatchers("/user/login").permitAll()
                 .requestMatchers("/post/search/**").permitAll()
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll() // swagger-ui는 허용해줘도 필터를 통과 못하는데 뭐임??
                 .anyRequest().authenticated()
                 // JWT 인증 사용하기 위한 설정
                 .and().addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
-        http.formLogin();
+//        http.formLogin();
 
         // 401 에러 핸들링
         http.exceptionHandling().authenticationEntryPoint(customAuthenticationEntryPoint);
